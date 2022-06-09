9716fdb62002a9efb10036b4777c53655c30f3ff
sadikhossain69 <sadikhossain433@gmail.com>
2022-06-09 21:17:25 +0600

    contacting email functionality added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,41 +1,58 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_kpgu2qj', 'template_1w9zbrw', form.current, 'izhjd_XATNZhThYrx')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
         <section>
-            <section className="text-gray-600 body-font relative">
-                <div className="container px-5 py-24 mx-auto">
-                    <div className="flex flex-col text-center w-full mb-12">
-                    <h2 className='text-center text-4xl font-bold underline underline-offset-8 decoration-blue-500 text-blue-500 mb-5'>Contact Me</h2>
-                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
-                    </div>
-                    <div className="lg:w-1/2 md:w-2/3 mx-auto">
-                        <div className="flex flex-wrap -m-2">
-                            <div className="p-2 w-1/2">
-                                <div className="relative">
-                                    <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
-                                    <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            <form ref={form} onSubmit={sendEmail}>
+                <section className="text-gray-600 body-font relative">
+                    <div className="container px-5 py-24 mx-auto">
+                        <div className="flex flex-col text-center w-full mb-12">
+                            <h2 className='text-center text-4xl font-bold underline underline-offset-8 decoration-blue-500 text-blue-500 mb-5'>Contact Me</h2>
+                            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+                        </div>
+                        <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                            <div className="flex flex-wrap -m-2">
+                                <div className="p-2 w-1/2">
+                                    <div className="relative">
+                                        <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
+                                        <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="p-2 w-1/2">
-                                <div className="relative">
-                                    <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-                                    <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                                <div className="p-2 w-1/2">
+                                    <div className="relative">
+                                        <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+                                        <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="p-2 w-full">
-                                <div className="relative">
-                                    <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
-                                    <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                <div className="p-2 w-full">
+                                    <div className="relative">
+                                        <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
+                                        <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="p-2 w-full">
-                                <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                                <div className="p-2 w-full">
+                                    <button type='submit' className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </form>
         </section>
     );
 };
