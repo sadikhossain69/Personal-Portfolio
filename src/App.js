3d22978b5079f2bb9af21696331cdb25c45d9b31
sadikhossain69 <sadikhossain433@gmail.com>
2022-06-09 20:20:20 +0600

    my skills section done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from '../src/components/Hero'
 import Footer from './components/Footer';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div>
       <Hero/>
+      <Skills/>
       <Footer/>
     </div>
     // #2D2B3B
---
+++ src/components/Skills.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <section className='my-10'>
+            <h2 className='text-center text-4xl font-bold underline underline-offset-8 decoration-green-500 text-green-500 mb-10'>My Skills</h2>
+            <div className='grid grid-cols-1 gap-5 md:px-10'>
+                <div>
+                    <h2 className='text-3xl font-bold'>Expertise:</h2>
+                    <p className='space-x-3 space-y-1 my-2'>
+                        <div className="badge badge-outline font-medium">Html</div>
+                        <div className="badge badge-outline font-medium">Css</div>
+                        <div className="badge badge-outline font-medium">Tailwind Css</div>
+                        <div className="badge badge-outline font-medium">Bootstrap 5</div>
+                        <div className="badge badge-outline font-medium">Javascript</div>
+                        <div className="badge badge-outline font-medium">React</div>
+                        <div className="badge badge-outline font-medium">React Router</div>
+                        <div className="badge badge-outline font-medium">Daisy Ui</div>
+                    </p>
+                </div>
+                <div>
+                    <h2 className='text-3xl font-bold'>Comfortable:</h2>
+                    <p className='space-x-3 space-y-1 my-2'>
+                        <div className="badge badge-outline font-medium">Express</div>
+                        <div className="badge badge-outline font-medium">MongoDB</div>
+                        <div className="badge badge-outline font-medium">Payment Gateway</div>
+                    </p>
+                </div>
+                <div>
+                    <h2 className='text-3xl font-bold'>Familiar:</h2>
+                    <p className='space-x-3 space-y-1 my-2'>
+                        <div className="badge badge-outline font-medium">React Native</div>
+                        <div className="badge badge-outline font-medium">Next Js</div>
+                        <div className="badge badge-outline font-medium">Material Ui</div>
+                    </p>
+                </div>
+                <div>
+                    <h2 className='text-3xl font-bold'>Tools</h2>
+                    <p className='space-x-3 space-y-1 my-2'>
+                        <div className="badge badge-outline font-medium">Github</div>
+                        <div className="badge badge-outline font-medium">Vs Code</div>
+                        <div className="badge badge-outline font-medium">Heroku</div>
+                        <div className="badge badge-outline font-medium">Firebase Authentication</div>
+                        <div className="badge badge-outline font-medium">Chrome Dev Tool</div>
+                        <div className="badge badge-outline font-medium">Netlify</div>
+                        <div className="badge badge-outline font-medium">Postman</div>
+                        <div className="badge badge-outline font-medium">Figma</div>
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Skills;
