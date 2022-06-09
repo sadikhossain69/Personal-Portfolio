0df8687928abffc17bb2002e029cd81639444435
sadikhossain69 <sadikhossain433@gmail.com>
2022-06-09 20:58:03 +0600

    contact form desing ready

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Hero from '../src/components/Hero'
 import Footer from './components/Footer';
 import Skills from './components/Skills';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div>
       <Hero/>
       <Skills/>
+      <Contact/>
       <Footer/>
     </div>
     // #2D2B3B
