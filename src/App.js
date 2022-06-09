44a770750560faf57b2f371cd25b49b9a59f0498
sadikhossain69 <sadikhossain433@gmail.com>
2022-06-09 18:37:01 +0600

    footer and hero section 50% done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
+import Hero from '../src/components/Hero'
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world! 
-      </h1>
-      dfsdf
+      <Hero/>
+      <Footer/>
     </div>
     // #2D2B3B
   );
